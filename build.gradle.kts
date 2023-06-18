41bd21ff556d1fd19e6b792744d24ebfdd98c658
fedmag <maggiorefederico@outlook.com>
2023-06-18 18:48:52 +0200

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,11 @@ dependencies {
     implementation("com.google.cloud:google-cloud-speech:4.14.0")
 
     compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    // logging
+    implementation("ch.qos.logback:logback-classic:1.2.9")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
