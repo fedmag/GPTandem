f50282f423ef36593d2bdb49a14e4b3f88d5eb71
fedmag <maggiorefederico@outlook.com>
2023-06-24 21:59:26 +0200

    added logback config and ConfigManager for other configuration

--- src/main/java/fedmag/gptandem/GPTandem.java
+++ src/main/java/fedmag/gptandem/GPTandem.java
@@ -1,6 +1,7 @@
 package fedmag.gptandem;
 import fedmag.gptandem.services.helper.Languages;
 import fedmag.gptandem.controller.Controller;
+import fedmag.gptandem.utility.ConfigManager;
 import lombok.extern.slf4j.Slf4j;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -12,6 +13,7 @@ public class GPTandem {
         log.info("Hello and welcome!");
         log.info("Creating empty chat history..");
 
+        ConfigManager configManager = ConfigManager.getInstance();
         Controller controller = new Controller(Languages.GERMAN);
 
     }
